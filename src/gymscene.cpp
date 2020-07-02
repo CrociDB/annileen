3c70b53e58ba4e6043411f9c93fb45fc213ded2a
Bruno Croci <bruno.croci@touchpress.com>
2020-07-02 22:55:58 +0200

    loading new test meshes to gym scene

--- src/gymscene.cpp
+++ src/gymscene.cpp
@@ -13,25 +13,46 @@ void GymScene::start()
     meshMaterial->loadTextures();
 
     ModelLoader loader;
-    auto statueMeshes = loader.loadMesh("../../assets/models/statue.obj");
-    for (auto& m : statueMeshes)
+    for (auto& m : loader.loadMesh("../../assets/models/dragon.obj"))
     {
         if (m != nullptr)
         {
             m->transform.position = glm::vec3(0.0, 0.0, 0.0);
-            m->transform.scale = glm::vec3(0.03, 0.03, 0.03);
+            m->transform.scale = glm::vec3(0.8, 0.8, 0.8);
             m->setMaterial(meshMaterial);
             this->addMesh(m);
         }
     }
 
-    getCamera()->transform.position = glm::vec3(-3.0f, 0.0f, -2.0f);
+    for (auto& m : loader.loadMesh("../../assets/models/bunny.obj"))
+    {
+        if (m != nullptr)
+        {
+            m->transform.position = glm::vec3(1.2, -0.2, 0.0);
+            m->transform.scale = glm::vec3(0.5, 0.5, 0.5); 
+            m->setMaterial(meshMaterial);
+            this->addMesh(m);
+        }
+    }
+
+    for (auto& m : loader.loadMesh("../../assets/models/head.obj"))
+    {
+        if (m != nullptr)
+        {
+            m->transform.position = glm::vec3(0.3, 0.0, 1.4);
+            m->transform.scale = glm::vec3(1.8, 1.8, 1.8);
+            m->setMaterial(meshMaterial);
+            this->addMesh(m);
+        }
+    }
+
+    getCamera()->transform.position = glm::vec3(-3.0f, 0.0f, -1.0f);
 
     Light* light = new Light();
     light->color = glm::vec3(0.5f, 0.6f, 1.0f);
     light->type = LightDirectional;
     light->intensity = 0.6f;
-    light->transform.rotate(glm::vec3(0.0f, 90.0f, 0.0f));
+    light->transform.rotate(glm::vec3(-35.0f, 75.0f, 0.0f));
 
     this->addLight(light);
 }
