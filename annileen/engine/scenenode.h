cbd75fe8be16a4a5b98b19f864d343d5f6d1f6ba
Bruno Croci <crocidb@gmail.com>
2020-09-01 23:02:25 +0200

    moving template implementations to the end of the scenenode header

--- annileen/engine/scenenode.h
+++ annileen/engine/scenenode.h
@@ -53,93 +53,97 @@ namespace annileen
 		bool hasChild(SceneNodePtr node);
 		void _destroyNode(SceneNodePtr node);
 
-		template <class T>
-		T* getModule() const
-		{
-			auto moduleIt = m_Modules.find(typeid(T));
-
-			if (moduleIt != m_Modules.end())
-			{
-				return dynamic_cast<T*>(moduleIt->second);
-			}
-
-			return nullptr;
-		}
-
-		template <class T>
-		bool addModule(T* module)
-		{
-			SceneNodeModulePtr sceneNodeModule = dynamic_cast<SceneNodeModulePtr>(module);
-
-			if (sceneNodeModule == nullptr)
-			{
-				//ANNILEEN_LOGF_ERROR(LoggingChannel::General, "\"{0}\" cannot be added because it is not a module.", typeid(T).name());
-				return false;
-			}
-
-			auto moduleIt = m_Modules.find(typeid(T));
-
-			if (moduleIt != m_Modules.end())
-			{
-				//ANNILEEN_LOG_ERROR(LoggingChannel::General, "This SceneNode has a Module of this type already. Remove the existing module before adding a new one of the same type.");
-				return false;
-			}
-
-			m_Modules[typeid(T)] = module;
-
-			module->m_SceneNode = this;
-
-			// "if constexpr" is a C++17 thing.
-			if constexpr (std::is_same<T, Camera>::value)
-			{
-				m_ParentScene->m_Cameras.push_back(module);
-			}
-			else if constexpr (std::is_same<T, Light>::value)
-			{
-				m_ParentScene->m_Lights.push_back(module);
-			}
-
-			return true;
-		}
-
-		template <class T>
-		bool removeModule()
-		{
-			auto moduleIt = m_Modules.find(typeid(T));
-
-			if (moduleIt == m_Modules.end())
-			{
-				//ANNILEEN_LOG_ERROR(LoggingChannel::General, "This SceneNode does not have a Module of this type.");
-				return false;
-			}
-
-
-			T* module = dynamic_cast<T*>(moduleIt->second);
-
-			if (module != nullptr)
-			{
-
-				module->m_SceneNode = nullptr;
-
-				if constexpr (std::is_same<T, Camera>::value)
-				{
-					m_ParentScene->m_Cameras.remove(module);
-				}
-				else if constexpr (std::is_same<T, Light>::value)
-				{
-					m_ParentScene->m_Lights.remove(module);
-				}
-
-				delete module;
-			}
-
-			m_Modules.erase(moduleIt);
-
-			return true;
-		}
+		template <class T> T* getModule() const;
+		template <class T> bool addModule(T* module);
+		template <class T> bool removeModule();
 
 		SceneNode();
 		~SceneNode();
 	};
+
+	// Template implementations
+
+	template <class T>
+	T* SceneNode::getModule() const
+	{
+		auto moduleIt = m_Modules.find(typeid(T));
+
+		if (moduleIt != m_Modules.end())
+		{
+			return static_cast<T*>(moduleIt->second);
+		}
+
+		return nullptr;
+	}
+
+	template <class T>
+	bool SceneNode::addModule(T* module)
+	{
+		SceneNodeModulePtr sceneNodeModule = static_cast<SceneNodeModulePtr>(module);
+
+		if (sceneNodeModule == nullptr)
+		{
+			//ANNILEEN_LOGF_ERROR(LoggingChannel::General, "\"{0}\" cannot be added because it is not a module.", typeid(T).name());
+			return false;
+		}
+
+		auto moduleIt = m_Modules.find(typeid(T));
+
+		if (moduleIt != m_Modules.end())
+		{
+			//ANNILEEN_LOG_ERROR(LoggingChannel::General, "This SceneNode has a Module of this type already. Remove the existing module before adding a new one of the same type.");
+			return false;
+		}
+
+		m_Modules[typeid(T)] = module;
+
+		module->m_SceneNode = this;
+
+		// "if constexpr" is a C++17 thing.
+		if constexpr (std::is_same<T, Camera>::value)
+		{
+			m_ParentScene->m_Cameras.push_back(module);
+		}
+		else if constexpr (std::is_same<T, Light>::value)
+		{
+			m_ParentScene->m_Lights.push_back(module);
+		}
+
+		return true;
+	}
+
+	template <class T>
+	bool SceneNode::removeModule()
+	{
+		auto moduleIt = m_Modules.find(typeid(T));
+
+		if (moduleIt == m_Modules.end())
+		{
+			//ANNILEEN_LOG_ERROR(LoggingChannel::General, "This SceneNode does not have a Module of this type.");
+			return false;
+		}
+
+		T* module = static_cast<T*>(moduleIt->second);
+
+		if (module != nullptr)
+		{
+			module->m_SceneNode = nullptr;
+
+			if constexpr (std::is_same<T, Camera>::value)
+			{
+				m_ParentScene->m_Cameras.remove(module);
+			}
+			else if constexpr (std::is_same<T, Light>::value)
+			{
+				m_ParentScene->m_Lights.remove(module);
+			}
+
+			delete module;
+		}
+
+		m_Modules.erase(moduleIt);
+
+		return true;
+	}
 
 }
