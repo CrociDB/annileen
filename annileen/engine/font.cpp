acf2562827c69354d6db399c7b5ad0bc9585a2cc
Teo <teofilo.dutra@gmail.com>
2020-09-13 20:23:55 -0700

    - Cleaned up font class.

--- annileen/engine/font.cpp
+++ annileen/engine/font.cpp
@@ -3,74 +3,29 @@
 
 namespace annileen
 {
-	bx::FileReaderI* Font::s_fileReader = nullptr;
-	bx::AllocatorI* Font::g_allocator = &s_allocator;
-	bx::DefaultAllocator Font::s_allocator;
-	Font::String Font::s_currentDir;
-	
-
-	bx::FileReaderI* Font::getFileReader()
+	Font::Font(std::string filename) 
 	{
-		if (s_fileReader == nullptr)
+		// TODO: move this to File class.
+		std::ifstream file;
+		file.open(filename, std::ifstream::binary | std::ifstream::in);
+
+		file.seekg(0, std::ios::end);
+		size_t size = file.tellg();
+		file.seekg(0, std::ios::beg);
+
+		uint8_t* data = new uint8_t[size];
+		file.read(reinterpret_cast<char*>(data), size);
+		file.close();
+
+		if (data != nullptr)
 		{
-			s_fileReader = BX_NEW(&s_allocator, FileReader);
-		}
-
-		return s_fileReader;
-	}
-
-	void* Font::load(const char* _filePath, uint32_t* _size)
-	{
-		return load(getFileReader(), &s_allocator, _filePath, _size);
-	}
-
-	void* Font::load(bx::FileReaderI* _reader, bx::AllocatorI* _allocator, const char* _filePath, uint32_t* _size)
-	{
-		if (bx::open(_reader, _filePath))
-		{
-			uint32_t size = (uint32_t)bx::getSize(_reader);
-			void* data = BX_ALLOC(_allocator, size);
-			bx::read(_reader, data, size);
-			bx::close(_reader);
-			if (NULL != _size)
-			{
-				*_size = size;
-			}
-			return data;
+			m_Handle = ServiceProvider::getFontManager()->createTtf(data, static_cast<uint32_t>(size));
+			delete[] data;
 		}
 		else
 		{
-			std::cerr << "Failed to open: " << _filePath << std::endl;
+			m_Handle = BGFX_INVALID_HANDLE;
 		}
-
-		if (NULL != _size)
-		{
-			*_size = 0;
-		}
-
-		return NULL;
-	}
-
-	TrueTypeHandle Font::loadTtf(FontManager* _fm, const char* _filePath)
-	{
-		uint32_t size;
-		void* data = load(_filePath, &size);
-
-		if (NULL != data)
-		{
-			TrueTypeHandle handle = _fm->createTtf((uint8_t*)data, size);
-			BX_FREE(&s_allocator, data);
-			return handle;
-		}
-
-		TrueTypeHandle invalid = BGFX_INVALID_HANDLE;
-		return invalid;
-	}
-
-
-	Font::Font(std::string filename) 
-	{
-		m_Handle = loadTtf(ServiceProvider::getFontManager(), filename.c_str());
 	}
 
 	Font::~Font()
--- annileen/engine/font.h
+++ annileen/engine/font.h
@@ -3,20 +3,6 @@
 #include <string>
 #include <iostream>
 
-#include <bx/bx.h>
-#include <bx/allocator.h>
-#include <bgfx/bgfx.h>
-
-#include <bgfx/bgfx.h>
-#include <bx/commandline.h>
-#include <bx/endian.h>
-#include <bx/math.h>
-#include <bx/readerwriter.h>
-#include <bx/string.h>
-#include <bx/debug.h>
-#include <bx/file.h>
-#include <unordered_map>
-
 #include <engine/asset.h>
 #include <engine/text/fontmanager.h>
 
@@ -25,33 +11,6 @@ namespace annileen
 	class Font final : public AssetObject
 	{
 	private:
-
-		static bx::DefaultAllocator s_allocator;
-		static bx::FileReaderI* s_fileReader;
-
-		static bx::AllocatorI* g_allocator;
-		typedef bx::StringT<&g_allocator> String;
-
-		static String s_currentDir;
-
-		class FileReader : public bx::FileReader
-		{
-			typedef bx::FileReader super;
-
-		public:
-			virtual bool open(const bx::FilePath& _filePath, bx::Error* _err) override
-			{
-				String filePath(s_currentDir);
-				filePath.append(_filePath);
-				return super::open(filePath.getPtr(), _err);
-			}
-		};
-
-		bx::FileReaderI* getFileReader();
-		void* load(const char* _filePath, uint32_t* _size);
-		void* load(bx::FileReaderI* _reader, bx::AllocatorI* _allocator, const char* _filePath, uint32_t* _size);
-		TrueTypeHandle loadTtf(FontManager* _fm, const char* _filePath);
-
 		TrueTypeHandle m_Handle;
 	public:
 		const TrueTypeHandle& getHandle() const { return m_Handle; }
