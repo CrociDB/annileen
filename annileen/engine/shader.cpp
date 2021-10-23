c2ee97372477813e2c6bc45beeea59ec81489379
Bruno Croci <crocidb@gmail.com>
2021-10-23 18:03:20 +0200

    cleaning shader code

--- annileen/engine/shader.cpp
+++ annileen/engine/shader.cpp
@@ -19,45 +19,5 @@ namespace annileen
     {
         m_ProgramHandle = handle;
     }
-
-    void Shader::use()
-    {
-        //glUseProgram(m_ShaderProgram);
-    }
-
-    void Shader::setBool(const std::string& name, bool value)
-    {
-        //glUniform1i(glGetUniformLocation(m_ShaderProgram, name.c_str()), (int)value);
-    }
-
-    void Shader::setInt(const std::string& name, int value)
-    {
-        //glUniform1i(glGetUniformLocation(m_ShaderProgram, name.c_str()), value);
-    }
-
-    void Shader::setFloat(const std::string& name, float value)
-    {
-        //glUniform1f(glGetUniformLocation(m_ShaderProgram, name.c_str()), value);
-    }
-
-    void Shader::setVec2(const std::string& name, const glm::vec2& value)
-    {
-        //glUniform2f(glGetUniformLocation(m_ShaderProgram, name.c_str()), value.x, value.y);
-    }
-
-    void Shader::setVec3(const std::string& name, const glm::vec3& value)
-    {
-        //glUniform3f(glGetUniformLocation(m_ShaderProgram, name.c_str()), value.x, value.y, value.z);
-    }
-
-    void Shader::setVec4(const std::string& name, const glm::vec4& value)
-    {
-        //glUniform4f(glGetUniformLocation(m_ShaderProgram, name.c_str()), value.x, value.y, value.z, value.w);
-    }
-
-    void Shader::setMat4(const std::string& name, const glm::mat4& value)
-    {
-        //glUniformMatrix4fv(glGetUniformLocation(m_ShaderProgram, name.c_str()), 1, GL_FALSE, glm::value_ptr(value));
-    }
 }
 
--- annileen/engine/shader.h
+++ annileen/engine/shader.h
@@ -19,17 +19,8 @@ namespace annileen
 
     public:
         void init(bgfx::ProgramHandle handle);
-        void use();
 
         bgfx::ProgramHandle getProgram() { return m_ProgramHandle; }
-    
-        void setBool(const std::string& name, bool value);
-        void setInt(const std::string& name, int value);
-        void setFloat(const std::string& name, float value);
-        void setVec2(const std::string& name, const glm::vec2& value);
-        void setVec3(const std::string& name, const glm::vec3& value);
-        void setVec4(const std::string& name, const glm::vec4& value);
-        void setMat4(const std::string& name, const glm::mat4& value);
 
         Shader();
         ~Shader();
